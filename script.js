f02a84a6c667c70fe9b599cce7cfd51db0fe6127
Abhimanyu sable <ambys281990@gmail.com>
2022-02-14 15:38:44 +0000

    Changes made in code

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = p*years*rate/100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result").innerHTML = "<br> If you deposit <mark>" + p + "</mark> <br />" +
+                                                  "at an interest rate of <mark>" + rate + "</mark> % <br />" +
+                                                  "You will receive an amount of <mark>" + interest + "</mark>, <br />" + 
+                                                  "in the year <mark>" + year + "</mark>"; 
+
+
 }
-        
+
+function updateRate() {
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
+}
+
+function ValidateInput(){
+    var Principal = document.getElementById("principal").value;
+
+    if (Principal > 0) {
+        console.log("Value is positive");
+    }
+
+    else if (Principal <= 0) {
+        console.log("The number is negative");
+        alert("Enter a positive value for principal");
+        document.getElementById("principal").focus();
+    }
+
+}
